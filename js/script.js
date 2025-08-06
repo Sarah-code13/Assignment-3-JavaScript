b5bb26148ce75f0f2e19e1be8976f817034860b2
Sarah Perdomo <sarahdanielap@gmail.com>
2025-08-05 23:54:31 -0400

    assignment 3 1/3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-const baseUrl = "https://api.spoonacular.com/";
+const baseUrl = "https://api.spoonacular.com";
 const api = "04d1b52789974969a4b74a06107f2a63";
 const input = document.querySelector(".input");
 const searchBtn = document.querySelector(".searchBtn");
-
+const nutritionBox = document.querySelector(".nutritionBox");
+const calories = document.querySelector(".calories");
+const protein = document.querySelector(".protein");
+const carbs = document.querySelector(".carbs");
+const fat = document.querySelector(".fat");
 const resultPara = document.createElement("p");
-document.appendChild(resultPara);
+resultPara.className = "resultPara";
+document.body.appendChild(resultPara);
 const container = document.createElement("div");
-document.appendChild(container);
+document.body.appendChild(container);
+container.className = "container"
 searchBtn.addEventListener("click", () => {
 const query = input.value.trim();
     if (!query) {
@@ -16,16 +22,18 @@ const query = input.value.trim();
 
     resultPara.textContent = "API is in function";
     container.innerHTML = "";
+    
 
-    fetch(`${baseUrl}/recipes/complexSearch?query=${query}&number=1&apikey=${api}`)
+    fetch(`${baseUrl}/recipes/complexSearch?query=${query}&number=1&apiKey=${api}`)
     .then(res => res.json())
     .then(data => {
         resultPara.textContent = "";
-        if (!data.container.length) {
+        if (!data.results) {
             resultPara.textContent = "No food found"
             return;
         }
-        const item = data.container[0];
+        const item = data.results[0];
+       
         displayRecipe(item);
     })
 });
@@ -38,4 +46,28 @@ function displayRecipe(item) {
     image.style.maxWidth = "400px";
 
     const nutritionBtn = document.createElement("button");
+    nutritionBtn.textContent = "Show nutrition facts about the dish";
+    nutritionBtn.addEventListener("click", () =>
+    fetchNutritionfacts(item.id));
+
+    container.appendChild(title);
+    container.appendChild(image);
+    container.appendChild(nutritionBtn);
+}
+function fetchNutritionfacts(itemId) {
+    resultPara.textContent = "Retrieving nutrition facts from api";
+
+    fetch(`${baseUrl}/recipes/${itemId}/nutritionWidget.json?apiKey=${api}`)
+    .then(res =>
+        res.json()
+    ).then (data=> {
+       resultPara.textContent = "";
+       displayNutrition(data);
+    })
+}
+function displayNutrition(data){
+    calories.textContent = `Calories: ${data.calories} kcal`;
+    protein.textContent = `Proteins: ${data.protein} grams`;
+    carbs.textContent = `Carbohydrates: ${data.carbs} grams`;
+    fat.textContent = `Fat: ${data.fat} grams`;
 }
